f8899312a670369a46f6a83eb9f98b2a80a88ae4
KittenAmogus <kittenamogus3@gmail.com>
2026-01-06 21:47:41 +0300

    noone reads ts

--- server/server.py
+++ server/server.py
@@ -1,44 +1,32 @@
+import logging
 import socket
 import json
-
 
 from codes import CardCodes, PlayerCodes, ServerCodes
 from card import Card
 
 
-class LogLevels:
-	ERROR		=	0
-	EXCEPTION	=	1
-	INFO		=	2
-	DEBUG		=	3
+LOG_LEVEL		=	logging.DEBUG	# logging.INFO
+# LOG_PATH		=	"./Logs"
 
-	@staticmethod
-	def stringFromLevel( lvl ):
-		match ( lvl ):
-			case 0:	return "ERROR"
-			case 1:	return "EXCEPTION"
-			case 2:	return "INFO"
-			case 3:	return "DEBUG"
-			case _:	return "-"
+# from os import mkdir, path
+# from datetime import datetime
 
+# if ( not path.exists( LOG_PATH ) ):
+# 	mkdir( LOG_PATH )
 
-LOG_LEVEL_PRINT	= LogLevels.INFO
-LOG_LEVEL_FILE	= LogLevels.DEBUG
+# # 'DD.MM.YYYY HH:MM:SS.MS' to 'HH:MM:SS'
+# time = str( datetime.now() ).split()[ 1 ].split( '.' )[ 0 ]
+# LOG_FILE		=	f"{ LOG_PATH.removesuffix( '/' ) }/" f"server-{ time }.log"
+LOG_FILE	=	"log-server.log"
 
+# del mkdir, datetime
 
-def log( level, msg ):
-	match ( level ):
-		case ( LogLevels.ERROR ):		color = "5;101;30"
-		case ( LogLevels.EXCEPTION ):	color = "91"
-		case ( LogLevels.INFO ):		color = "93"
-		# case ( "debug" ):		color = "0"
-		case _:					color = "0"
-	
-	if ( LOG_LEVEL_PRINT >= level ):
-		print( f"\x1b[{ color }m[{ LogLevels.stringFromLevel(level) }]:",
-				msg, "\x1b[0m" )
-	if ( LOG_LEVEL_FILE >= level ):
-		...  # save to file
+LOG_FORMAT		=	'[%(levelname)s] (%(asctime)s) %(message)s'
+
+logger			=	logging.getLogger( __name__)
+logging.basicConfig( level=LOG_LEVEL, filename=LOG_FILE, format=LOG_FORMAT )
+logger.info( "\n\r--- New Session ---\n\r" )
 
 
 class Server:
@@ -56,49 +44,105 @@ class Server:
 		self.players		=	[]
 
 		self.socket			=	None
+
 	
 	# --- Server ---
 
-	def createServer( self ):
+	def _createServer( self ):
 		self.socket	=	socket.socket( socket.AF_INET, socket.SOCK_STREAM )
 		self.socket.setsockopt( socket.SOL_SOCKET, socket.SO_REUSEADDR, 1 )
+		logger.debug( "Created server" )
 
 		try:
 			self.socket.bind(( self.host, self.port ))
+			logger.debug( "Server bind success" )
 			self.socket.listen( self.max_clients )
-			log( LogLevels.INFO, f"Listen on: { self.host }:{ self.port }" )
+			logger.debug( "Server listen success" )
+			logger.info( f"Server listen on { self.host }:{ self.port }" )
 
 		except OSError as e:
-			log( LogLevels.ERROR, f"Cannot bind/listen: { e }" )
+			logger.error( f"Cannot bind/listen server: { e }" )
 			return False
 
-	def stopServer( self ):
+	def _stopServer( self ):
 		self.socket.detach()
 		self.socket.close()
-		log( LogLevels.INFO, "Server closed" )
+		logger.info( "Closed server" )
 	
 	# --- Client ---
 
-	def playerJoined( self ):
+	def _onPlayerJoined( self ):
 		pass
 	
-	def playerLeft( self ):
+	def _onPlayerLeft( self ):
 		pass
 	
-	def playerTurn( self ):
+	def _onPlayerTurn( self ):
 		pass
+	
+	def _waitPlayer( self ):
+		try:
+			sock, addr = self.socket.accept()
+			logger.info( f"User connected: { addr[ 0 ] }:{ addr[ 1 ] }" )
+			return sock
+
+		except OSError as e:
+			logger.error( f"Server connecting error: { e }" )
+			raise e
+
+		except ConnectionError:
+			logger.error( f"User connection error: { e }" )
+			return None
+
+		return None
 	
 	# --- Game ---
 
-	def placeCard( self ):
+	def _onPlaceCard( self ):
 		pass
 	
-	def drawCard( self ):
+	def _onDrawCard( self ):
 		pass
+
+
+class Game( Server ):
+	def __init__( self ):
+		super().__init__()
 	
+	# --- Server ---
+
+	def _startGame( self ):
+		self._createServer()
+	
+	def _stopGame( self ):
+		self._stopServer()
+	
+	# --- Players ---
+
+	def _waitForPlayers( self ):
+		while ( len( self.players ) < self.max_clients ):
+			sock	=	self._waitPlayer()
+			if ( sock is None ):
+				continue
+
+			self.players.append( sock )
+
+	# --- Start ---
+	def start( self ):
+		self._startGame()
+
+		try:
+			self._waitForPlayers()
+	
+		except KeyboardInterrupt:
+			logger.debug( "Received Ctrl+C" )
+
+		finally:
+			self._stopGame()
+
 
 def main():
-	...
+	Game().start()
 
 if __name__ == "__main__":
 	main()
